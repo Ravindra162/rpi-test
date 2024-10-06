787e0f2870fc8fdba5c8492880cbc47c241bf96d
Ravindra162 <tarakaravindra242005@gmail.com>
2024-10-06 12:30:26 +0530

    a

--- index.js
+++ index.js
@@ -1,45 +1,34 @@
-import express from "express"
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-import {createServer} from "http"
-import {Server} from "socket.io"
-
-const server = createServer(app)
-
-const io = new Server(server,{
+const server = createServer(app);
+const io = new Server(server, {
     cors: {
         origin: "*",
     }
-})
+});
 
+app.get("/", (req, res) => {
+    res.send("Hello from API");
+});
 
-app.get("/",(req,res)=>{
+io.on("connection", (socket) => {
+    console.log("User connected - ", socket.id);
 
-    res.send("Hello from api");
-})
+    socket.on("order", (data) => {
+        console.log(data);
+        io.emit("message", data);
+    });
 
+    socket.on("disconnect", () => {
+        console.log("User disconnected - ", socket.id);
+    });
+});
 
-app.post("/",(req,res)=>{
-    const data = req.body
-    console.log(data)
-    res.send({
-        data: "processed "+data,
-    })
-})
-
-io.on("connection",(socket)=>{
-    console.log("User connected - ",socket.id)
-    socket.on("message",(data)=>{
-        console.log(data)
-        io.emit("message",data)
-    })
-})
-
-
-
-
-server.listen(port,()=>{
-    console.log("Server is running on port "+port)
-})
+server.listen(port, () => {
+    console.log("Server is running on port " + port);
+});
